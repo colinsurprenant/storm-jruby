54c5d6544eb933fc214dc0110be5989395f1a4ab
Colin Surprenant <colin.surprenant@gmail.com>
2011-11-07 16:46:01 -0500

    storm-jruby is now redstorm

--- lib/red_storm.rb
+++
@@ -1,6 +0,0 @@
-module RedStorm
-  REDSTORM_HOME = File.expand_path(File.dirname(__FILE__) + '/..') unless defined?(REDSTORM_HOME)
-end
-
-require 'red_storm/version'
-require 'red_storm/application'
--- lib/red_storm/version.rb
+++
@@ -1,3 +0,0 @@
-module RedStorm
-  VERSION = '0.1.0'
-end
